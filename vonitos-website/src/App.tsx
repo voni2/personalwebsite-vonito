25f61194f234c6489b591cdad9ab875be41239e5
von <vongat16@gmail.com>
2025-05-02 09:34:53 +0800

    added Selectors and Visibility functionality

--- vonitos-website/src/App.tsx
+++ vonitos-website/src/App.tsx
@@ -2,9 +2,46 @@ import { useState } from 'react'
 import Header from './Header'
 import './App.css'
 import Footer from './Footer'
+import Selector from './assets/Hover-Selector.png'
 
 function App() {
- 
+  const [isHovered1, setIsHovered1] = useState(false);
+  const handleMouseEnterS1 = () => {
+    setIsHovered1(true);
+  };
+  const handleMouseLeaveS1 = () => {
+    setIsHovered1(false);
+  };
+  const SelectorVisibility1 = isHovered1 ? 'visible' : 'hidden';
+///////////////////////////////////////////////////////////////////
+  const [isHovered2, setIsHovered2] = useState(false);
+  const handleMouseEnterS2 = () => {
+    setIsHovered2(true);
+  };
+  const handleMouseLeaveS2 = () => {
+    setIsHovered2(false);
+  };
+  const SelectorVisibility2 = isHovered2 ? 'visible' : 'hidden';
+///////////////////////////////////////////////////////////////////
+  const [isHovered3, setIsHovered3] = useState(false);
+  const handleMouseEnterS3 = () => {
+    setIsHovered3(true);
+  };
+  const handleMouseLeaveS3 = () => {
+    setIsHovered3(false);
+  };
+  const SelectorVisibility3 = isHovered3 ? 'visible' : 'hidden';
+///////////////////////////////////////////////////////////////////
+  const [isHovered4, setIsHovered4] = useState(false);
+  const handleMouseEnterS4 = () => {
+    setIsHovered4(true);
+  };
+  const handleMouseLeaveS4 = () => {
+    setIsHovered4(false);
+  };
+  const SelectorVisibility4 = isHovered4 ? 'visible' : 'hidden';
+///////////////////////////////////////////////////////////////////
+
 
   return (
     <>
@@ -12,11 +49,27 @@ function App() {
  
  
   
-    <div className = "Menu">
-        <button> Continue </button>
-        <button> Character </button>
-        <button> Skills </button>
-        <button> About </button>
+    <div className='Menu'>
+      <table>
+      <tr>
+      <th><img src={Selector}  style={{ visibility: SelectorVisibility1 }}/></th>
+      <th> <img src={Selector} style={{ visibility: SelectorVisibility2 }}/></th>
+      <th> <img src={Selector} style={{ visibility: SelectorVisibility3 }}/></th>
+      <th> <img src={Selector} style={{ visibility: SelectorVisibility4 }}/></th>
+      </tr>
+
+
+      
+
+      <tr>
+       <th> <button onMouseEnter={handleMouseEnterS1} onMouseLeave={handleMouseLeaveS1}> Continue </button></th>
+       <th> <button onMouseEnter={handleMouseEnterS2} onMouseLeave={handleMouseLeaveS2}> Character </button></th>
+       <th>  <button onMouseEnter={handleMouseEnterS3} onMouseLeave={handleMouseLeaveS3}> Skills </button></th>
+       <th>  <button onMouseEnter={handleMouseEnterS4} onMouseLeave={handleMouseLeaveS4}> About </button></th>
+      </tr>
+
+      </table>
+    
     </div>
 
     
